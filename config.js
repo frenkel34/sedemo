bfad8697ce5fd61f0d93a5f1a278efa9ceeb7bd7
Frank <benusfrank@gmail.com>
2016-12-13 13:41:11 +0100

    Config file expanded

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ app_text_title			= 'Okta EMEA SE Demo';
 app_text_login			= 'Please fill in your username and password';
 app_color_background	= 'fff';
 app_color_primary		= '000';
+app_debug_show			= true;
 
 // Facebook settings
 social_fb_enable		= true;
@@ -17,7 +18,7 @@ social_fb_clientid		= 'eiXCFnbZE1LzT7ahtOwH';
 social_fb_scope			= 'openid%20email%20profile';
 
 // Google Plus settings
-social_gp_enable		= false;
+social_gp_enable		= true;
 social_gp_idp			= '0oad3gqm0nkUSd9uJ0i6';
 social_gp_clientid		= 'VD495qEPPsYluex6tj6G';
 social_gp_scope			= 'openid%20email%20profile';
