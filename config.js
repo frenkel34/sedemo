7dc7c10a35d39ffa539fb26cb30e79134a2b387b
Frank <benusfrank@gmail.com>
2016-12-13 13:08:58 +0100

    Added config and look and feel

---
+++ config.js
@@ -0,0 +1,31 @@
+// Javascript config file for Okta native portal app
+// Author: Frank Benus
+
+// Default values
+okta_Org_Url 			= 'http://randomcompany.okta-emea.com';
+
+// App look and feel
+app_text_title			= 'Okta EMEA SE Demo';
+app_text_login			= 'Please fill in your username and password';
+app_color_background	= 'fff';
+app_color_primary		= '000';
+
+// Facebook settings
+social_fb_enable		= true;
+social_fb_idp			= '0oacy8jkfxWnxwgPn0i6';
+social_fb_clientid		= 'eiXCFnbZE1LzT7ahtOwH';
+social_fb_scope			= 'openid%20email%20profile';
+
+// Google Plus settings
+social_gp_enable		= false;
+social_gp_idp			= '0oad3gqm0nkUSd9uJ0i6';
+social_gp_clientid		= 'VD495qEPPsYluex6tj6G';
+social_gp_scope			= 'openid%20email%20profile';
+
+// LinkedIn settings
+social_li_enable		= true;
+social_li_idp			= '0oacyj8ypAB4J4gr30i6';
+social_li_clientid		= 'QGSed7lDqb73VA6UrrXw';
+social_li_scope			= 'openid%20email%20profile';
+
+
